3388f6fdb86169adbb3d1b42cb4cdd934b13b46f
Johan <rawbington@gmail.com>
2015-04-13 01:51:03 +0200

    Lägga upp filer test

---
+++ Hemsida/src/group7/pvt/dsv/su/se/SimpleServlet.java
@@ -0,0 +1,50 @@
+package group7.pvt.dsv.su.se;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet("/SimpleServlet")
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SimpleServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String userName = request.getParameter("userName");
+        userName = Test.getName(userName);
+        out.println("Hello from POST method! " + userName);
+	}
+
+}
---
+++ Hemsida/src/group7/pvt/dsv/su/se/Test.java
@@ -0,0 +1,6 @@
+package group7.pvt.dsv.su.se;
+public class Test {
+public static String getName(String inputValue){
+            return inputValue + " through Class!";
+}
+}
